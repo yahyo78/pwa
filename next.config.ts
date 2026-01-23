d9366f9d4710889c7b3f8b0fac833e92d6eda7dc
yahyo78 <yahyojanhasanov2007@gmail.com>
2026-01-23 14:05:03 +0500

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-  dest: 'public',
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
+  fallbacks: {
+    document: "/~offline", // Вақте ки саҳифа бор намешавад, инро нишон медиҳад
+  },
 });
 
 module.exports = withPWA({
-  reactStrictMode: true,
-  // You can add other Next.js config options here
+  // Танзимоти дигари Next.js
 });
